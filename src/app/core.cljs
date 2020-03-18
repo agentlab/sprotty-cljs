3ca82bf8db3bb15d21eb73ea371b13615068b441
Mvwivs <mvwivs@gmail.com>
2020-03-18 19:14:13 +0300

    test components

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -10,8 +10,11 @@
    [:h1 (:text @app-state)]
    [:h3 "Edit this and watch it change!"]])
 
+(defn some-component []
+  [:button {:on-click (fn [] (js/console.log "Click"))} "Help"]) 
+
 (defn start []
-  (reagent/render-component [hello-world]
+  (reagent/render-component [some-component]
                             (. js/document (getElementById "app"))))
 
 (defn ^:export init []
